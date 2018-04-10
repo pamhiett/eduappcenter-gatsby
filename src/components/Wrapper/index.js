9f1e019219b174eccc6931e0fb5df223ab40f13b
Arlo Guthrie <aguthrie@instructure.com>
2018-04-10 08:50:07 -0500

    block Container

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import Container from '@instructure/ui-container/lib/components/Container'
 
 const Wrapper = ({ as, children, ...rest }) => (
-  <Container as={as} {...rest}>
+  <Container display="block" as={as || 'div'} {...rest}>
     {/* TODO: How to use the maxWidth value in the theme? */}
     <div style={{ maxWidth: '60rem', margin: '0 auto' }}>{children}</div>
   </Container>
